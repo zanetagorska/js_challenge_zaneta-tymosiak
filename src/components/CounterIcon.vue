e76205eae84b359ad693727d64a4eb7120a54f09
Zaneta <zaneta.gorska1@gmail.com>
2021-01-22 10:45:10 +0100

    Feature: Created CouterIcons and display wishlist and cart count

---
+++ src/components/CounterIcon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="counter-icon">
+    <div><base-icon :name="name" /></div>
+    <div v-if="count" class="counter-icon__count">{{ count }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import BaseIcon from './BaseIcon.vue';
+
+export default {
+  name: 'CounterIcon',
+  components: { BaseIcon },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.counter-icon {
+  position: relative;
+  display: inline-block;
+  margin-right: 10px;
+}
+.counter-icon__count {
+  position: absolute;
+  bottom: 0;
+  right: -10px;
+  border-radius: 50%;
+  height: 16px;
+  width: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: pink;
+  font-size: 12px;
+}
+</style>
--- src/components/HeaderActions.vue
+++ src/components/HeaderActions.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <div>Wishlist products count: {{ wishlistCount }}</div>
-    <div>Cart items count: {{ cartCount }}</div>
-    <div>Cart total price: {{ cartTotalPrice }}</div>
+  <div class="header-actions">
+    <span class="header-actions__wishlist">
+      <counter-icon name="heart" :count="wishlistCount" />
+    </span>
+    <counter-icon name="shopping-bag" :count="cartCount" />
+    <span v-if="cartTotalPrice">{{ totalPrice }}</span>
   </div>
 </template>
 
@@ -10,13 +12,25 @@
 import { cartCount, cartTotalPrice, wishlishCount } from '@/types/Getter.types';
 import { defineComponent } from '@vue/composition-api';
 import { mapGetters } from 'vuex';
+import CounterIcon from './CounterIcon.vue';
 
 export default defineComponent({
+  components: { CounterIcon },
   name: 'HeaderActions',
   computed: {
     ...mapGetters([wishlishCount, cartCount, cartTotalPrice]),
+    totalPrice() {
+      return `${this.cartTotalPrice} zł`;
+    },
   },
 });
 </script>
-
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header-actions {
+  display: flex;
+  align-items: center;
+}
+.header-actions__wishlist {
+  margin-right: 10px;
+}
+</style>
